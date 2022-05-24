7ae510c078933fa90c8d8da723b93ad2d45b3f80
Santi <santiagohenn@gmail.com>
2022-05-24 16:52:56 -0300

    Removed unused code. Commented some blocks.

--- compute_LLA.m
+++ compute_LLA.m
@@ -107,55 +107,24 @@ alt = a - RE;
 
 end
 
+% This function computes the eccentric anomaly
+% function [E] = anom_ecc(M,e) 
+% Resolución numerica para la ecuación: E-e*sin(E)=M
+% E = anomalia eccentrica [rad]
+% e = excentricidad
+% M = anomalia media [rad]
+% El programa calcula E con un error de orden -10
+% Para mejorar la certeza modificar el valor de err
+
 function [E] = anom_ecc(M,e)
-    % function [E] = anom_ecc(M,e) 
-    % Risoluzione numerica dell'equazione: E-e*sin(E)=M
-    % E = anomalia eccentrica [rad]
-    % e = eccentricità
-    % M = anomalia media [rad]
-    % Si devono dare in input alla funzione due valori scalari,
-    % rispettivamente M in rad ed e.
-    % Il programma restituisce E [rad] con un errore di 1e-10
-    % N.B. Per migliorare l'accuratezza accedere all'editor e modificare il
-    % valore della variabile err, che rappresenta l'errore commesso.
-    
-    format long
-%     x = 0;
-%     sx = 1;
-%     dymax = -1;
-%     trovato = true;
-%     while (trovato)
-%         if (sx<0.2)
-%             sx = 0.1 - (x/1000);
-%         else
-%             sx  = M-x;
-%             dx  = M+x;
-%             dy  = abs(cos(dx));
-%             dy2 = abs(cos(sx));
-%         end
-%         if (dymax<dy || dymax<dy2)
-%             if (dy<dy2)
-%                 dymax = dy2;
-%             else
-%                 dymax = dy;
-%                 dy = dymax;
-%             end
-%         end 
-%         f0 = sx-e.*sin(sx)-M;
-%         f1 = dx-e.*sin(dx)-M;
-%         trovato = ((f0.*f1)>0);
-%         x = x + 0.1;
-%     end
+
     E = M;
     k = 1;
     err = 1e-10;
-    % stabilito intervallo di ricerca (sx,dx) e max valore derivata dymax;
+    % set search interval (sx, dx) and max derived value dymax
     while (k>err)
         y = e*sin(E)+M;
         k = abs(abs(E)-abs(y));
         E = y;
     end
-    % trovato E con un errore inferiore a 0.1*10^(-4);
-    %fprintf(' La soluzione E è stata trovata nell''intervallo [%2.5f,%2.5f]',sx,dx);
-    %fprintf(' \n errore inferiore a %1.2e: [rad] E = %3.5f',err,E);
 end
