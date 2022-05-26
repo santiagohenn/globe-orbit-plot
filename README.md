f1015623601599ca8261289178ce695fe44636f3
Santi <santiagohenn@gmail.com>
2022-05-26 12:28:34 -0300

    Modularized main script. README wip

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # globe-orbit-plot
-3D globe plots implemented in MATLAB, useful for demonstrations that need constellation plotting.
+
+Scripts to plot constellations in a 3D user interface. 
+
+### Usage:
+
+Input the constellation satellites via a .csv file, one for each line, grouped
+by planes. This is: if you have N planes with M satellites, the M 
+satellites from the first plane should come first, then the M satellites 
+for the second plane, and so on until the $N^{th}$ plane.
+
+CSV's columns are as follows:
+
+|  time  | a (Sem-Maj axis)  | ecc | inc | RAAN  | $\omega$  | $v_{0}$  |
+|---|---|---|---|---|---|---|
+
+Where time is the Greenwich Hour angle, a is in meters, and angles are in degrees. E.g.:
+
+```csv
+36.406,6978135,0,98,0,0,0
+36.406,6978135,0,98,0,0,45
+36.406,6978135,0,98,0,0,90
+36.406,6978135,0,98,0,0,135
+36.406,6978135,0,98,0,0,180
+36.406,6978135,0,98,0,0,225
+...
+```
+
+Then modify these lines in the orbit_plotter.m script:
+
+```Matlab
+% Read CSV file containing the constellation parameters
+con = csvread('PATH/TO/CONSTELLATION/CSV');
+
+% Set the number of planes and sats per plane
+nPlanes = 1;        % < ----- N. of planes
+nSatsInPlane = 8;   % < ----- N. of sats
+```
+
+And run 
--- orbit_plotter.m
+++ orbit_plotter.m
@@ -1,32 +1,23 @@
-clc
-format('shortG')
+%% Modify here VVVVVVVV
 
 % Read CSV file containing the constellation parameters
 con = csvread('constellation.csv');
 
 % Set the number of planes and sats per plane
-nPlanes = 1;        % < -----
-nSatsInPlane = 8;   % < -----
+nPlanes = 1;        % < ----- N. of planes
+nSatsInPlane = 8;   % < ----- N. of sats
+
+% Modify here ^^^^^^^^^
+
+clc
+format('shortG')
+
+% N of sats in constellation
 nSats = nPlanes * nSatsInPlane;
 
-% Obtain SSPs
-lla = zeros(nSats,3);
-
-for sat = 1 : nSats
-    
-    time = con(sat,1);
-    a = con(sat,2) / 1000;
-    e = con(sat,3);
-    i = con(sat,4);
-    W = con(sat,5);
-    w = con(sat,6);
-    v0 = con(sat,7);
-    [lat, lon, alt] = compute_LLA(time,a,e,i,W,w,v0);
-    lla(sat,:) = [lat, lon, alt];
-    
-end
-
 %% Plot ground tracks in 2D
+
+lla = elements2lla(con);
 
 for n = 1 : nSatsInPlane : nSats
    
@@ -36,15 +27,16 @@ for n = 1 : nSatsInPlane : nSats
     longsToPlot(nPlanes + 1) = longsToPlot(1);
     [latI,lonI] = interpm(latsToPlot,longsToPlot,0.1,'gc');
     geoplot(latI,lonI,'-','MarkerSize',12,'LineWidth',1,'Color',[1 0 1]); hold on;
-%     geoplot(lla(:,1),lla(:,2),'p','MarkerSize',5,'Color',[1 0 0]); hold on;
+    geoplot(lla(:,1),lla(:,2),'p','MarkerSize',5,'Color',[1 0 0]); hold on;
     
 end
 
-%% Plot in 3D
+%% Plot in 3D globe
+
+lla = elements2lla(con);
 
 uif = uifigure;
 g = geoglobe(uif,'Basemap','darkwater');
-
 satHeight = lla(n,3)*1e3;
 
 for n = 1 : nSatsInPlane : nSats
@@ -68,5 +60,25 @@ end
 
 geobasemap(g,'darkwater')
 
-%% Export current view as file
+%% Export globe-3D current view as file
 exportapp(uif,'snaps/snap.pdf');
+
+%% Function that obtains the lla matrix for the constellation from the .csv
+function lla = elements2lla(con)
+
+    nSats = size(con, 1);
+    lla = zeros(nSats,3);
+
+    for sat = 1 : nSats
+        time = con(sat,1);
+        a = con(sat,2) / 1000;
+        e = con(sat,3);
+        i = con(sat,4);
+        W = con(sat,5);
+        w = con(sat,6);
+        v0 = con(sat,7);
+        [lat, lon, alt] = compute_LLA(time,a,e,i,W,w,v0);
+        lla(sat,:) = [lat, lon, alt];
+    end
+    
+end
